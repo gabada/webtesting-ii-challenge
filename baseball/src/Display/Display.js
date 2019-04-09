5e1ee795cf05e2e13a0b98711eae846ecca445e3
Gill Abada <gill@callifi.com>
2019-04-09 19:10:34 -0400

    add another dependency, finish tests for balls and strikes.

--- baseball/src/Display/Display.js
+++ baseball/src/Display/Display.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+function Display(props) {
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>Balls: {props.balls}</p>
+      <p>Strikes: {props.strikes}</p>
+    </>
+  );
+}
+
+export default Display;
--- baseball/src/Display/Display.spec.js
+++ baseball/src/Display/Display.spec.js
@@ -1,4 +1,52 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 import Display from './Display';
+
+afterEach(cleanup);
+
+describe('<Display />', () => {
+  it('component renders', () => {
+    render(<Display />);
+  });
+  describe('balls', () => {
+    it('balls are displayed', () => {
+      const { getByText } = render(<Display />);
+      getByText(/balls/i);
+    });
+    it('count is correct', () => {
+      const { queryByText } = render(<Display balls={2} />);
+      expect(queryByText(/balls: 2/i)).not.toBeNull();
+    });
+    it('props are passed', () => {
+      const { queryByText } = render(<Display balls={1} />);
+      expect(queryByText(/balls: 1/i)).not.toBeNull();
+    });
+    // it.todo('count increments'); MOVE TO DASHBOARD
+    // it.todo('count resets when hit'); MOVE TO DASHBOARD
+    // it.todo('count resets when 4 balls are recorded'); MOVE TO DASHBOARD
+    // it.todo('count maxs out at 4'); MOVE TO DASHBOARD
+    // it.todo('foul counter is not touched'); MOVE TO DASHBOARD
+  });
+  describe('strikes', () => {
+    it('strikes are displayed', () => {
+      const { getByText } = render(<Display />);
+      getByText(/strikes/i);
+    });
+    it('count is correct', () => {
+      const { queryByText } = render(<Display strikes={2} />);
+      expect(queryByText(/strikes: 2/i)).not.toBeNull();
+    });
+    it('props are passed', () => {
+      const { queryByText } = render(<Display strikes={1} />);
+      expect(queryByText(/strikes: 1/i)).not.toBeNull();
+    });
+    // it.todo('count increments'); MOVE TO DASHBOARD
+    // it.todo('count resets when hit'); MOVE TO DASHBOARD
+    // it.todo('count resets when 3 strikes are recorded'); MOVE TO DASHBOARD
+    // it.todo('count maxs out at 3'); MOVE TO DASHBOARD
+    // it.todo('increase fouls by 1 when strikes are below 3'); MOVE TO DASHBOARD
+    // it.todo('with 2 strikes foul has no effect on fouls'); MOVE TO DASHBOARD
+  });
+});

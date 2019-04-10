6cb59cbe2e1231a34ddc50c90929ce3a32ab7d34
Gill Abada <gill@callifi.com>
2019-04-09 20:28:23 -0400

    clean up display tests and point App to Dashboard.

--- baseball/src/Display/Display.spec.js
+++ baseball/src/Display/Display.spec.js
@@ -23,11 +23,6 @@ describe('<Display />', () => {
       const { queryByText } = render(<Display balls={1} />);
       expect(queryByText(/balls: 1/i)).not.toBeNull();
     });
-    // it.todo('count increments'); MOVE TO DASHBOARD
-    // it.todo('count resets when hit'); MOVE TO DASHBOARD
-    // it.todo('count resets when 4 balls are recorded'); MOVE TO DASHBOARD
-    // it.todo('count maxs out at 4'); MOVE TO DASHBOARD
-    // it.todo('foul counter is not touched'); MOVE TO DASHBOARD
   });
   describe('strikes', () => {
     it('strikes are displayed', () => {
@@ -42,11 +37,5 @@ describe('<Display />', () => {
       const { queryByText } = render(<Display strikes={1} />);
       expect(queryByText(/strikes: 1/i)).not.toBeNull();
     });
-    // it.todo('count increments'); MOVE TO DASHBOARD
-    // it.todo('count resets when hit'); MOVE TO DASHBOARD
-    // it.todo('count resets when 3 strikes are recorded'); MOVE TO DASHBOARD
-    // it.todo('count maxs out at 3'); MOVE TO DASHBOARD
-    // it.todo('increase fouls by 1 when strikes are below 3'); MOVE TO DASHBOARD
-    // it.todo('with 2 strikes foul has no effect on fouls'); MOVE TO DASHBOARD
   });
 });
